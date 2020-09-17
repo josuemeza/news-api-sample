ed5f385fd2f612ec9d182898ffc70ed65c916666
Josue Meza <contacto@josuemeza.com>
2020-09-17 02:27:07 -0300

    Refactor on news service endpoints

--- src/services/news.service.js
+++ src/services/news.service.js
@@ -3,15 +3,12 @@ const { firestore } = require("../libs/firestore.lib")
 const News = require("../model/news")
 const { v4: uuidv4 } = require('uuid')
 
-const collectionId = "news"
+const collection = firestore.collection("news")
 const newsRouter = Router()
 
 newsRouter.route("/")
   .get(async (_request, response) => {
-    const collection = await firestore
-      .collection(collectionId)
-      .get()
-    const newsList = collection.docs
+    const newsList = await collection.get().docs
       .map( doc => new News(doc.data()) )
     response.json(newsList)
   })
@@ -22,8 +19,7 @@ newsRouter.route("/")
     }
     const news = new News(data)
     const json = news.json()
-    await firestore
-      .collection(collectionId)
+    await collection
       .doc(news.uuid)
       .set(json)
     response.status(201).json(json)
@@ -31,33 +27,29 @@ newsRouter.route("/")
 
 newsRouter.route("/:uuid")
   .get(async (request, response) => {
-    const uuid = request.params.uuid
-    const doc = await firestore
-      .collection(collectionId)
-      .doc(uuid)
+    const current = await collection
+      .doc(request.params.uuid)
       .get()
-    const news = new News(doc.data())
+    const news = new News(current.data())
     response.json(news)
   })
   .put(async (request, response) => {
+    const doc = collection
+      .doc(request.params.uuid)
+    const current = await doc.get()
     const data = {
-      uuid: request.params.uuid,
+      ...current.data(),
       ...request.body
     }
     const news = new News(data)
     news.updatedAt = new Date()
     const json = news.json()
-    await firestore
-      .collection(collectionId)
-      .doc(news.uuid)
-      .set(json)
+    await doc.set(json)
     response.json(json)
   })
   .delete(async (request, response) => {
-    const uuid = request.params.uuid
-    await firestore
-      .collection(collectionId)
-      .doc(uuid)
+    await collection
+      .doc(request.params.uuid)
       .delete()
     response.json({ message: "deleted" })
   })
