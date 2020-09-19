e9dabd26d01b17a399a410b27e5a9ff3ea040899
Josue Meza <hola@josuemeza.com>
2020-09-19 13:39:32 -0300

    Clean unused files

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-gcloud functions deploy $GCP_CF_NAME \
-  --entry-point $GCP_CF_ENTRY_POINT \
-  --runtime $GCP_CF_RUNTIME \
-  --region $GCP_CF_REGION \
-  --memory $GCP_CF_MEMORY \
-  --max-instances $GCP_CF_MAX_INSTANCES \
-  --set-env-vars  API_KEY=$APP_API_KEY \
-  --trigger-http
