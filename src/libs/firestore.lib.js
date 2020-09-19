3facb8b05ac4e23dacda3dd85ee580f5f7d94b21
Josue Meza <hola@josuemeza.com>
2020-09-19 12:44:45 -0300

    Fix firebase credentials

--- src/libs/firestore.lib.js
+++ src/libs/firestore.lib.js
@@ -1,7 +1,8 @@
 const admin = require("firebase-admin")
+const basepath = "../../"
 
 const initFirestore = () => {
-  const path = process.env.GOOGLE_APPLICATION_CREDENTIALS
+  const path = `${basepath}${process.env.CGP_DB_CREDENTIALS}`
   const serviceAccount = require(path)
   const credential = admin.credential.cert(serviceAccount)
   admin.initializeApp({ credential })
