3d7cfc87dfd0249b8eb9c5e9f775f1b8f73bc306
Josue Meza <hola@josuemeza.com>
2020-09-19 13:01:40 -0300

    Added env variable to google build

--- src/libs/firestore.lib.js
+++ src/libs/firestore.lib.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin")
 const basepath = "../../"
 
 const initFirestore = () => {
-  const path = `${basepath}${process.env.CGP_DB_CREDENTIALS}`
+  const path = `${basepath}${process.env.GCP_DB_CREDENTIALS}`
   const serviceAccount = require(path)
   const credential = admin.credential.cert(serviceAccount)
   admin.initializeApp({ credential })
